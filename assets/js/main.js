6f3b7795f5bed29920a09bfea53262348b087302
Geoff Gard <gardgeoff95@email.arizona.edu>
2019-06-12 14:35:26 -0700

    Everything except ratings

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,23 @@ $(document).ready(function () {
 
 
 
-    var awesomeShows = ["The Office", "Futurama", "Scrubs", "Firefly", "30 Rock", "Parks and Recreation",];
-    $("#submit").on("click", function(){
-        var textBox = $("#textBox").val()
-        if (textBox != ""){
-            awesomeShows.push(textBox);
-            console.log(awesomeShows);
-            
-        }
-    })
+    var awesomeShows = ["The Office", "Futurama", "Scrubs", "Firefly", "30 Rock", "Parks and Recreation", "Breaking Bad", "Dexter", "Spongebob", "House", "Friends", "Seinfeld", "The Simpsons"];
+
 
     var buttons = $("#buttonContainer");
     button = $("<button>")
+    function renderButtons() {
+        $(buttons).empty();
+        for (i = 0; i < awesomeShows.length; i++) {
 
-    for (i = 0; i < awesomeShows.length; i++) {
-        $(buttons).append("<button class = 'showButton' show-name='" + awesomeShows[i] + "'>" + awesomeShows[i] + "</button")
+            $(buttons).append("<button class = 'showButton' show-name='" + awesomeShows[i] + "'>" + awesomeShows[i] + "</button")
 
+        }
     }
+    renderButtons();
 
-    $(".showButton").on("click", function () {
+
+    $("body").on("click", ".showButton", function () {
 
 
         var show = $(this).attr('show-name');
@@ -33,18 +31,52 @@ $(document).ready(function () {
         }).then(function (response) {
 
             $("#gifs").empty();
+
+
             var results = response.data
+
+
+
             console.log(response);
             for (i = 0; i < results.length; i++) {
-                var img = $("<img>").attr("src", results[i].images.fixed_height.url);
+                var animated = results[i].images.fixed_height.url;
+                var still = results[i].images.fixed_height_still.url;
 
+                var img = $("<img>").attr("src", still).attr("state", "still").attr("animated", animated).attr("stillImg", still);
 
                 $("#gifs").prepend(img);
+
+
+
             }
+
 
 
         })
 
     })
-   
+
+
+
+    $("#submit").on("click", function () {
+        var textBox = $("#textBox").val().trim();
+
+        awesomeShows.push(textBox);
+        console.log(awesomeShows);
+        renderButtons()
+
+
+
+    })
+    $("body").on("click", "img", function () {
+        if ($(this).attr("state") == "still") {
+            $(this).attr("src", $(this).attr("animated"));
+            $(this).attr("state", "animated");
+        } else {
+            $(this).attr("src", $(this).attr("stillImg"))
+            $(this).attr("state", "still");
+        }
+    })
+
+
 });
